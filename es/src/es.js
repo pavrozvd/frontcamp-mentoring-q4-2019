23874fcf53564c9921873876fc157dd2c7b95e95
Владимир <pavrozvd@gmail.com>
2019-10-30 15:48:30 +0300

    es6 features

--- es/src/es.js
+++ es/src/es.js
@@ -1,15 +1,22 @@
-//
+class ApiCalls {
+  async getNews(channel = "ars-technica") {	
+    const URL = 'https://newsapi.org/v1/articles?source=' + channel + '&apiKey=d6f85eed1e60462e8d156f1030124aef';
+    const fetchResult = fetch(URL)
+    const response = await fetchResult;
+    return await response.json();
+  }
 
-async function getSources() {
-  const URL = 'https://newsapi.org/v1/sources';
-  const fetchResult = fetch(URL)
-  const response = await fetchResult;
-  return await response.json();
+  async getSources() {
+    const URL = 'https://newsapi.org/v1/sources';
+    const fetchResult = fetch(URL)
+    const response = await fetchResult;
+    return await response.json();
+  }
 }
 
-getSources().then(ret_val => {   
-   var i;
-   for (i = 0; i < ret_val.sources.length; i++) {	
+var apiCalls = new ApiCalls(); 
+apiCalls.getSources().then(ret_val => {   
+   for (let i = 0; i < ret_val.sources.length; i++) {	
    	var opt1 = document.createElement("option");
   	opt1.value = ret_val.sources[i].id;
  	  opt1.text =  ret_val.sources[i].name;
@@ -17,26 +24,25 @@ getSources().then(ret_val => {
    }
 })
 
-async function getNews(channel) {	
-  const URL = 'https://newsapi.org/v1/articles?source=' + channel + '&apiKey=d6f85eed1e60462e8d156f1030124aef';
-  const fetchResult = fetch(URL)
-  const response = await fetchResult;
-  return await response.json();
-}
 
 const selectElement = document.querySelector('#channels');
 selectElement.addEventListener('change', (event) => {
-  getNews(event.target.value).then(ret_val => {   
+  var apiCalls = new ApiCalls(); 
+
+  apiCalls.getNews(event.target.value).then(ret_val => {   
     var tableRef = document.getElementById('news').getElementsByTagName('tbody')[0];
     for(var i = tableRef.rows.length - 1; i > 0; i--)
     {
         tableRef.deleteRow(i);
     }
 
-    var i;
-    for (i = 0; i < ret_val.articles.length; i++) {	
+    for (let i = 0; i < ret_val.articles.length; i++) {	
       var newRow   = tableRef.insertRow(tableRef.rows.length);
-      newRow.innerHTML = "<tr><td>" + ret_val.articles[i].title + "</td><td>" + ret_val.articles[i].description + "</td><tr>";
+      var title = ret_val.articles[i].title;
+      var description = ret_val.articles[i].description;
+      obj = { title, description };
+
+      newRow.innerHTML = `<tr><td>${obj.title}</td><td>${obj.description}</td><tr>`;
     }
   })  
 });
